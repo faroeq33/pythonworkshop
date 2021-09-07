4d67ca5af5c44bdc6bd6d0d2e45dfec673399c45
Faroeq <alves_1044@hotmail.com>
2021-09-07 20:17:36 +0200

    add exercise 2

---
+++ exercise2.py
@@ -0,0 +1,20 @@
+# exercise 1.1
+print(type(6))
+
+# exercise 1.2
+print(type(6.0))
+
+# exercise 1.3
+print(5 + 3.14)
+
+
+# exercise 1.4
+# je kan een float omzetten naar een integer door de int functie te gebruiken
+print(int(7.9999999))
+
+# exercise 1.5
+# je kan een float omzetten naar een integer door de int functie te gebruiken
+print(float(6))
+
+# complex number types
+print((2 + 3j) / (1 - 5j))
