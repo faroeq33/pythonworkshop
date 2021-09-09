9f3984e480de6b6c969de06810402c2194a4fcd1
Faroeq <alves_1044@hotmail.com>
2021-09-09 17:18:55 +0200

    add exercise 11, 12

---
+++ exercises/exercise11.py
@@ -0,0 +1,12 @@
+typeOfFive = type('5')  # str
+
+# Dit zal uitkomen op 57, omdat het 5 en 7 aan elkaar plakt als tekst
+castingFiveAndSeven = '5' + '7'  # 57
+
+# Maak van 5 een integer
+intFive = int('5')
+
+# Hiermee kunt je 5 en 7 met elkaar optellen
+addFiveAndSeven = int('5') + int('7')
+
+print(addFiveAndSeven)
---
+++ exercises/exercise12.py
@@ -0,0 +1,8 @@
+# 12.2
+print('What is your name?')
+
+# 12.3
+name = input()
+
+# 12.4
+print('Hello, ' + name + '.')
