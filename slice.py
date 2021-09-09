03af72baf7a347afe7debeb0138d7b5d54449858
Faroeq <alves_1044@hotmail.com>
2021-09-09 17:56:19 +0200

    add slice exercises

---
+++ slice.py
@@ -0,0 +1,11 @@
+destination = 'San Francisco'
+
+thingOne = destination[4:11]  # Francis
+
+thingTwo = destination[0:3]  # San
+
+thingThree = destination[:8]
+# San Fran, dit hakt 1e letter tot en met de laatste letter inclusief de spatie
+thingFour = destination[:-3]  # pakt totaan laatste 3 letters
+
+print(thingFour)
