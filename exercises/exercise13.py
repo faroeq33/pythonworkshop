f2e9840212e6398721a4930612f642c1b9097b03
Faroeq <alves_1044@hotmail.com>
2021-09-10 12:38:22 +0200

    add rest of comparison operator in exercise 13

--- exercises/exercise13.py
+++ exercises/exercise13.py
@@ -1,5 +1,11 @@
 over_18 = True
+
 print(type(over_18))
 
 over_21 = False
-print(type(over_21))
+
+over_18, over_21 = True, False
+
+print(over_18 and over_21)
+print(over_18 or over_21)
+print(not over_21 or (over_21 or over_18))
