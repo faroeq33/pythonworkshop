ffc77e5d377c8aa55ef7dc1ae224eaece109380d
Faroeq <alves_1044@hotmail.com>
2021-09-10 13:27:07 +0200

    add exercise16

---
+++ exercises/exercise16.py
@@ -0,0 +1,13 @@
+# 16.2
+age = 20
+if age >= 18 and age < 21:
+    print('At least you can vote.')
+    print('Poker will have to wait.')
+else:
+    print('welcome to our voting progam')
+# 16.3
+
+if age >= 18:
+    print('Welcome to our voting program.')
+else:
+    print('You aren\'t old enough to vote.')
