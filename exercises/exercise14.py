a7c3e8f1bc5254d12d48406361f4c7efc2a7b531
Faroeq <alves_1044@hotmail.com>
2021-09-10 12:50:44 +0200

    add exercise 14

---
+++ exercises/exercise14.py
@@ -0,0 +1,10 @@
+age = 20
+isLessThanThirteen = age < 13
+isNotEqualToTwentyOne = age != 21
+isEqualToNineteen = age == 19
+sixEqualToSixDotO = 6 == 6.0
+
+print(sixEqualToSixDotO)
+print(6 == '6')
+print((age >= 20 and age < 30) or (age >= 30 and age < 40))
+print((20 <= age < 30) or (30 <= age < 40))
