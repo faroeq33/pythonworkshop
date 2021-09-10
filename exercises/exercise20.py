8ba4f7820a27236f0489299f3504d68c4f587e49
Faroeq <alves_1044@hotmail.com>
2021-09-10 18:55:02 +0200

    add exercise20.py

---
+++ exercises/exercise20.py
@@ -0,0 +1,18 @@
+# geeft het woord portlang verticaal weer
+for i in 'Portland':
+    print(i)
+
+for thing in range(1, 10):
+    print(thing)
+
+# telt in stappen van 2
+for i in range(1, 11, 2):
+    print(i)
+
+for i in range(3, 0, -1):
+    print(i)
+
+name = 'faroeq'
+for i in range(0, 3):
+    for i in name:
+        print(i)
