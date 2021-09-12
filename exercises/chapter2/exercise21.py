640386dc98e510870eff39af7f3d9f61911ceb7d
Faroeq <alves_1044@hotmail.com>
2021-09-12 12:30:43 +0200

    remove editorconfig, add exercise 21,22

---
+++ exercises/chapter2/exercise21.py
@@ -0,0 +1,12 @@
+shopping = ["bread", "milk", "eggs"]
+print(shopping)
+
+# display mixed items
+for item in shopping:
+    print(item)
+
+mixed = [365, "days", True]
+
+# print mixed items
+for item in mixed:
+    print(item)
