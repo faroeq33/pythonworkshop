49720d671d1b45311948a3c2f3be506034354f91
Faroeq <alves_1044@hotmail.com>
2021-09-14 11:54:46 +0200

    add activity 6

---
+++ activities/chapter2/activity6.py
@@ -0,0 +1,12 @@
+employees = [["Lisa Crawford", 29, "Marketing"], [
+    "John Mckee", 38, "Sales"], ["Sujan Patel", 33, "HR"], ]
+
+lisa = employees[0]
+print(lisa)
+print("Name:", lisa[0])
+print("Age:", lisa[1])
+print("Department:", lisa[2])
+print("-"*20)
+# print(employees[0][0])
+# print(employees[0][1])
+# print(employees[0][2])
