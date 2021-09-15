9ae586281f0b229e9029bb365ac64382ca999a22
Faroeq <alves_1044@hotmail.com>
2021-09-15 14:49:47 +0200

    add exercise24.py

---
+++ exercises/chapter2/exercise24.py
@@ -0,0 +1,16 @@
+x = [[1, 2], [4, 5], [3, 6]]
+y = [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+result = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+# iterating by row of x
+for i in range(len(x)):
+    # iterating by column by y
+    for j in range(len(y[0])):
+        # iterating by rows of y
+        for k in range(len(y)):
+            result[i][j] += x[i][k] * y[k][j]
+
+
+for r in result:
+    print(r)
