8f42b7a566051c8370a44703bba82b93fa94c6d9
Faroeq <alves_1044@hotmail.com>
2021-09-17 16:24:51 +0200

    add exercise 28

---
+++ exercises/chapter2/exercise28.py
@@ -0,0 +1,20 @@
+movie = {
+    "title": "The Godfather",
+    "director": "Francis Lord Coppola",
+    "year": 1972,
+    "rating": 9.2,
+}
+# print('year:', movie['year'])
+
+# update dictionary value
+movie['rating'] = (movie['rating'] + 9.3)/2
+
+# print('rating:', movie['rating'])
+
+movieTwo = {}
+movieTwo['title'] = "The godfather"
+movieTwo['director'] = "Francis Ford coppola"
+movieTwo['year'] = 1972
+movieTwo['rating'] = 9.2
+
+print(movieTwo)
