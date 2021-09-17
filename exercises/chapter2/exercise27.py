205f5dbba204961d812a1e92a5c5eed5e8564e07
Faroeq <alves_1044@hotmail.com>
2021-09-17 15:31:35 +0200

    add exercise 27

---
+++ exercises/chapter2/exercise27.py
@@ -0,0 +1,14 @@
+# shopping = ["bread", "milk", "eggs"]
+# shopping.append("apple")
+# print(shopping)
+
+
+# with the append method you can add items to a list
+shopping = []
+shopping.append('bread')
+shopping.append('milk')
+shopping.append('eggs')
+shopping.append('apple')
+print(shopping)
+
+shopping.insert(2, 'ham')  # adds string after 3rd index
