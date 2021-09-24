be59ac82901eccb7486d1565a28796e1a4a81f0e
Faroeq <alves_1044@hotmail.com>
2021-09-24 16:55:16 +0200

    fix mistake in exercise55.py

--- exercises/chapter3/exercise55.py
+++ exercises/chapter3/exercise55.py
@@ -1,6 +1,18 @@
-# def add_up(x, y):
-#     return x+y
+# first item in a list
+def first_item(my_list): return my_list[0]
 
-add_up = lambda x, y: x + y
 
-print(add_up(2, 5))  # 7
+first_item(['cat', 'dog', 'mouse'])
+
+# mapping
+names = ['Magda', 'Jose', 'Anne']
+
+lengths = []
+for name in names:
+    lengths.append(len(name))
+
+lengths = list(map(len, names))
+
+lengthOfYourPP = sum(lengths) / len(lengths)
+
+print(lengthOfYourPP)
