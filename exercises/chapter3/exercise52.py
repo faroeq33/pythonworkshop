1c2691d29f6c607884e88600ccb03f1ea6d10c94
Faroeq <alves_1044@hotmail.com>
2021-09-24 15:15:09 +0200

    add exercise 52

---
+++ exercises/chapter3/exercise52.py
@@ -0,0 +1,19 @@
+stored_results = {}
+
+
+def sum_to_n(n):
+    result = 0
+    for i in reversed(range(n)):
+        if i + 1 in stored_results:
+            print(
+                'Stopping sum at %s because we have previously computed it' % str(i + 1))
+            result += stored_results[i + 1]
+            break
+        else:
+            result += i + 1
+    stored_results[n] = result
+    return result
+
+
+print("result", sum_to_n(5))
+print("result", sum_to_n(6))
