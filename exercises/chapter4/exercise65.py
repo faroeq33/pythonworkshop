69372c32f0ed912c3a5d41761e86dfeff0bd56a0
Faroeq <alves_1044@hotmail.com>
2021-09-26 13:57:42 +0200

    exercise65.py

---
+++ exercises/chapter4/exercise65.py
@@ -0,0 +1,25 @@
+# Plotting
+import matplotlib.pyplot as plt
+
+labels = ['Monica', 'Adrian', 'Jared']
+
+num = [230, 100, 98]  # Note that this does not need to be percentages
+
+plt.pie(
+    num,
+    labels=labels,
+    autopct='%1.1f%%',
+    colors=['lightblue', 'lightgreen',
+            'yellow']
+)
+
+plt.title('Voting Results: Club President', fontdict={'fontsize': 20})
+
+plt.pie(
+    num,
+    labels=labels,
+    autopct='%1.1f%%',
+    colors=['lightblue', 'lightgreen',
+            'yellow']
+)
+plt.show()
