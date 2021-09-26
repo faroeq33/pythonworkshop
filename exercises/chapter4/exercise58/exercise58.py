78a61d45ec10232160c00d626b7c5661019309e0
Faroeq <alves_1044@hotmail.com>
2021-09-26 11:07:21 +0200

    add exercise 58 an 59

---
+++ exercises/chapter4/exercise58/exercise58.py
@@ -0,0 +1,3 @@
+file = open('pg37431.txt')
+text = file.read()
+print(text)
---
+++ exercises/chapter4/exercise59/exercise59.py
@@ -0,0 +1,5 @@
+with open("pg37431.txt") as f:
+    # read first 5 characters
+    print(f.read(5))
+
+    print(f.readline())
